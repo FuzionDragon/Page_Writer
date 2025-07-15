6d01c4c189e94e6cb06cb8e1efcf9f543dc8d32c
David L <120467347+FuzionDragon@users.noreply.github.com>
2025-07-15 18:53:10 +0100

    Rendering of snippets and notes added, some styling added to ensure input page (or home, don't have a name properly set) to not have any scrolling and is fixed

--- src/view.js
+++ src/view.js
@@ -2,13 +2,21 @@ import { invoke } from '@tauri-apps/api/core';
 import { marked } from 'marked';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  let snippet = document.getElementById('snippet');
+  let snippet_container = document.getElementById('snippet');
 
   const corpus = await invoke('load_snippets')
     .catch((error) => console.log("error caught:" + error));
 
   console.log("From app: " + corpus);
   for (const [key, value] of Object.entries(corpus)) {
-    console.log(key + value);
+    const card = document.createElement('div');
+    card.className = "card";
+    const title = document.createElement('h2');
+    title.textContent = key;
+    const content = document.createElement('p')
+    content.textContent = value;
+    card.appendChild(title);
+    card.appendChild(content);
+    snippet_container.appendChild(card);
   }
 });
